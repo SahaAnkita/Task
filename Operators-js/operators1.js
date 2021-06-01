667d79dcca473154426f7bd0da2894e22ff72f97
SahaAnkita <sahaankita757@gmail.com>
2021-06-01 23:32:10 +0530

    operators learning

---
+++ Operators-js/operators1.js
@@ -0,0 +1,80 @@
+/* 1. 
+Take two numbers and perform all the arithmetic operations over them.*/
+
+// addition 
+
+let number1 = 5;
+let number2 = 2;
+let add = number1 + number2;
+
+console.log(add);
+
+// subtraction
+
+let number3 = 10;
+let number4 = 5;
+let subtraction = number3 - number4;
+
+console.log(subtraction);
+
+// multiplication
+
+let number5 = 10;
+let number6 = 5;
+let multiplication = number5 * number6;
+
+console.log(multiplication);
+
+// division
+
+let number7 = 10;
+let number8 = 5;
+let division = number7 / number8;
+
+console.log(division);
+
+// modulus / remainder
+
+let number9 = 14;
+let number10 = 3;
+let modulus = number9 % number10;
+
+console.log(modulus);
+
+// exponentiation
+
+let number11 = 3;
+let number12 = 2;
+let exponentiation = number11 ** number12;
+
+console.log(exponentiation);
+
+// increment 
+// preincrement
+
+let number13 = 3;
+let preincrement = ++number13;
+
+console.log(preincrement);
+
+// postincrement
+
+let number14 = 2;
+let postincrement = number13++;
+
+console.log(postincrement);
+
+// decrement 
+// predecrement
+
+let number15 = 3;
+let predecrement = --number15;
+
+console.log(predecrement);
+
+// postdecrement
+
+let number16 = 2;
+let postdecrement = number16--;
+
+console.log(postdecrement);
---
+++ Operators-js/operators2.js
@@ -0,0 +1,56 @@
+/*2. 
+Take 2 numbers and 2 strings and perform addition operation on them in same sequence.*/
+
+let a = 2;
+let b = 6;
+let c ="Ankita";
+let d = "Is"
+
+let result = a + b + c + d;
+
+document.write("<h1>Result is: "+result+"</h1>");
+
+/*3. 
+Take 2 Strings and 2 numbers and perform addition operation on them in same sequence.*/
+
+let a1 = "Ankita";
+let b1 = "Is";
+let c1 = 2;
+let d1 = 6;
+
+let result1 = a1 + b1 + c1 + d1;
+
+document.write("<h2>Result is: "+result1+"</h2>");   //all the things converted into string
+
+/*4.
+ Write an operation to get its result as NaN.*/
+
+
+
+
+/*5. 
+Take 2 boolean variables as true and false and perform And and Or logical operation over them and print the result as well as operation*/
+let a2 = 3;
+let b2 = -2;
+let c2 = 4;
+let d2 = 5;
+
+console.log(a2 > 0 && b2 > 0);
+console.log(a2 > 0 || b2 > 0);
+console.log(c2 > 0 && d2 > 0);
+console.log(c2 > 0 || d2 > 0);
+
+/*6. 
+Take 2 variable and compare them using == and ===, such that result of both the comparision operations is not equal.*/
+
+let a3 = 10;
+let a4 = 20;
+let a5 = 10;
+
+document.write(a3 == a4);
+document.write(a3 === a4);
+document.write(a3 == a5);
+document.write(a3 === a5);
+
+
+
