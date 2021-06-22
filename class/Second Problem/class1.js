fad42e4dc3f1b639f8c19be289da5e43e0342d1d
SahaAnkita <sahaankita757@gmail.com>
2021-06-22 13:52:42 +0530

    class problems solved

--- class/Second Problem/class1.js
+++ class/Second Problem/class1.js
@@ -1,2 +1,17 @@
 /*2. 
 Use the class in index file to create few employees and use the functions in class on them*/
+
+
+export default function Class1(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let employees = [];
+employees.push(new Class1('Ankita', 26));
+employees.push(new Class1('Chhaya', 17));
+employees.push(new Class1('Samir', 45));
+employees.push(new Class1('Purabi', 50));
+employees.push(new Class1('Ananya', 60));
+
+console.log(employees);
---
+++ class/Second Problem/index.js
@@ -0,0 +1,6 @@
+/*2. 
+Use the class in index file to create few employees and use the functions in class on them*/
+
+import Class1 from "./class1.js";
+
+console.log(Class1);
