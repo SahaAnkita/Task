535772ee03d060d872a7a98e3911b8c008098ab7
SahaAnkita <sahaankita757@gmail.com>
2021-06-22 22:43:26 +0530

    class problems solved

--- class/Second Problem/class1.js
+++ class/Second Problem/class1.js
@@ -1,17 +1,20 @@
 /*2. 
 Use the class in index file to create few employees and use the functions in class on them*/
 
-
-export default function Class1(name, age) {
-    this.name = name;
-    this.age = age;
+export default class Employee {
+    constructor(name, age, id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
 }
-
-let employees = [];
-employees.push(new Class1('Ankita', 26));
-employees.push(new Class1('Chhaya', 17));
-employees.push(new Class1('Samir', 45));
-employees.push(new Class1('Purabi', 50));
-employees.push(new Class1('Ananya', 60));
-
-console.log(employees);
+export function employeeName(employees, ab) {
+    employees.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase())
+            return -1;
+        if (a.name.toLowerCase() > b.name.toLowerCase())
+            return 1;
+        return 0;
+    })
+    return employees;
+}
--- class/Second Problem/index.js
+++ class/Second Problem/index.js
@@ -1,6 +1,13 @@
 /*2. 
 Use the class in index file to create few employees and use the functions in class on them*/
 
-import Class1 from "./class1.js";
+import Employee, {employeeName} from "./class1.js";
 
-console.log(Class1);
+let employees = [];
+employees.push(new Employee('Ankita', 26, 1));
+employees.push(new Employee('Chhaya', 17, 2));
+employees.push(new Employee('Samir', 45, 3));
+employees.push(new Employee('Purabi', 50,4));
+employees.push(new Employee('Ananya', 60, 5));
+
+console.log(employeeName(employees, 'name'));
