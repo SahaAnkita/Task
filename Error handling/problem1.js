698be3d7d3506fca27f6eefb6a3b94f749b5a5e6
SahaAnkita <sahaankita757@gmail.com>
2021-06-30 13:20:13 +0530

    error handling first problem solved

---
+++ Error handling/problem1.js
@@ -0,0 +1,39 @@
+/*1.
+Divide the employee age by null and handle the error. Also if the error occurs divide it by 2 and print it.*/
+
+let employeesJson = [
+    {
+        "name": "Ankita",
+        "age": 26
+    },
+    {
+        "name": "Chhaya",
+        "age": 40
+    },
+    {
+        "name": "Samir",
+        "age": 45
+    },
+    {
+        "name": "Purabi",
+        "age": 50
+    },
+    {
+        "name": "Ananya",
+        "age": 60
+    }
+]
+
+let len = Object.keys(employeesJson).length;
+
+for (let i = 0; i < len; i++) {
+    try {
+        console.log(`Dividing the employee age by null`)
+            ((employeesJson[i].age) / null)
+    } 
+    catch (e) {
+        console.log(`Previous age is  ${employeesJson[i].age}`);
+        let result = employeesJson[i].age / 2;
+        console.log(`After dividing age by 2 is ${result}`);
+    }
+}
