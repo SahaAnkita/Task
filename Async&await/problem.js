cec6ba420db8ad4413bd425c705340e4c79e1a91
SahaAnkita <sahaankita757@gmail.com>
2021-07-26 17:45:28 +0530

    async-await problem soving now

---
+++ Async&await/problem.js
@@ -0,0 +1,67 @@
+// Create a function that returns array of employees from a random time from 1 to 2 seconds using async await.
+const employees = [{
+        name: "Ankita",
+        age: 26,
+        id: 1,
+        salary: 510000
+    },
+    {
+        name: "Chhaya",
+        age: 40,
+        id: 2,
+        salary: 520000
+    },
+    {
+        name: "Samir",
+        age: 45,
+        id: 3,
+        salary: 525000
+    },
+    {
+        name: "Purabi",
+        age: 50,
+        id: 4,
+        salary: 515000
+    },
+    {
+        name: "Ananya",
+        age: 60,
+        id: 5,
+        salary: 530000
+    }
+]
+
+async function funcEmp() {
+    let funcEmp1 = new Promise(function (resolve, reject) {
+
+        if (employees != undefined) {
+            console.log(`Employees are present`)
+            resolve();
+        } else {
+            console.log(`Employees are not present`)
+            reject();
+        }
+
+    })
+    let printEmployees = await funcEmp1
+    console.log(printEmployees)
+}
+
+
+
+funcEmp().then(function () {
+    console.log(`present`)
+    new Promise(function (resolve, reject) {
+        let i = 0;
+        const interval = setInterval(() => {
+            console.log(employees[i]);
+            i++;
+            if (i >= employees.length) {
+                clearInterval(interval)
+            }
+        }, 1000)
+    })
+}).catch(function () {
+    console.log(`not present`)
+})
+
