6fa8a0c9c36752569f5cdc6817b6bc18d9c11841
SahaAnkita <sahaankita757@gmail.com>
2021-11-10 17:19:48 +0530

    indentation issue solved

--- React-crud/src/Components/Employee.jsx
+++ React-crud/src/Components/Employee.jsx
@@ -4,53 +4,53 @@ import { Modal } from "react-bootstrap";
 import EditForm from "./EditForm";
 
 const Employee = ({ employee }) => {
-	const { deleteEmployee } = useContext(EmployeeContext);
+  const { deleteEmployee } = useContext(EmployeeContext);
 
-	const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-	const handleShow = () => setShow(true);
-	const handleClose = () => setShow(false);
-	useEffect(() => {
-		handleClose();
-	}, [employee]);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  useEffect(() => {
+    handleClose();
+  }, [employee]);
 
-	return (
-		<>
-			<td>{employee.name}</td>
-			<td>{employee.age}</td>
-			<td>{employee.address}</td>
-			<td>{employee.gender}</td>
-			<td>{employee.about}</td>
-			<td>
-				<img className="profile-img" src={employee.image} alt="" />
-			</td>
-			<td>
-				<button
-					onClick={handleShow}
-					className="btn text-warning icon-btn"
-					data-toggle="modal"
-				>
-					<i className="material-icons">&#xE254;</i>
-				</button>
-				<button
-					onClick={() => deleteEmployee(employee.id)}
-					className="btn text-danger icon-btn"
-					data-toggle="modal"
-				>
-					<i className="material-icons">&#xE872;</i>
-				</button>
-			</td>
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>Edit Employee</Modal.Title>
-				</Modal.Header>
+  return (
+    <>
+      <td>{employee.name}</td>
+      <td>{employee.age}</td>
+      <td>{employee.address}</td>
+      <td>{employee.gender}</td>
+      <td>{employee.about}</td>
+      <td>
+        <img className="profile-img" src={employee.image} alt="" />
+      </td>
+      <td>
+        <button
+          onClick={handleShow}
+          className="btn text-warning icon-btn"
+          data-toggle="modal"
+        >
+          <i className="material-icons">&#xE254;</i>
+        </button>
+        <button
+          onClick={() => deleteEmployee(employee.id)}
+          className="btn text-danger icon-btn"
+          data-toggle="modal"
+        >
+          <i className="material-icons">&#xE872;</i>
+        </button>
+      </td>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Employee</Modal.Title>
+        </Modal.Header>
 
-				<Modal.Body>
-					<EditForm theEmployee={employee} />
-				</Modal.Body>
-			</Modal>
-		</>
-	);
+        <Modal.Body>
+          <EditForm theEmployee={employee} />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
 
 export default Employee;
